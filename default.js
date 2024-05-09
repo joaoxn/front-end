7d7ecc87c393770bfd6a0f7e700798da6c3319b9
João Victor <152796948+joaoxn@users.noreply.github.com>
2024-05-09 00:25:36 -0300

    Atualizar o default.js

--- default.js
+++ default.js
@@ -5,5 +5,9 @@ function updateResizeBased() {
     content = document.querySelector("#content-wrap")
     footer = document.querySelector(".footer")
     
-    content.style.paddingBottom = window.getComputedStyle(footer).height
+    content.style.paddingBottom = window.getComputedStyle(footer).height + window.computedStyleNumber(content, paddingTop) + "px"
+}
+
+function computedStyleNumber(element, styleProperty) {
+    return window.getComputedStyle(element)[styleProperty]
 }
