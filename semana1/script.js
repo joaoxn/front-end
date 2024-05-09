95bb3cba14495673aac4f989f5c646bf2d894f2f
João Victor <jvso@yahoo.com>
2024-05-09 17:15:09 -0300

    Forms made

---
+++ semana1/script.js
@@ -0,0 +1,7 @@
+document.querySelector("#quiz").addEventListener('submit', (event) => {
+    event.preventDefault();
+})
+
+function enviar() {
+    document.querySelector("#quiz").children = "";
+}
