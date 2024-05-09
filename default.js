5c72528708038394ee8bac5d83c75203ddf95559
João Victor <152796948+joaoxn@users.noreply.github.com>
2024-05-09 08:02:02 -0300

    Atualizar o default.js

--- default.js
+++ default.js
@@ -1,13 +1,15 @@
-updateResizeBased()
+updateResizeBased();
 window.addEventListener('resize', updateResizeBased);
 
 function updateResizeBased() {
-    content = document.querySelector("#content-wrap")
-    footer = document.querySelector(".footer")
+    content = document.querySelector("#content-wrap");
+    footer = document.querySelector(".footer");
     
-    content.style.paddingBottom = window.getComputedStyle(footer, height) + window.computedStyleNumber(content, paddingTop) + "px"
+    const newHeight = window.getComputedStyle(footer, height) + window.computedStyleNumber(content, paddingTop) + "px";
+    
+    content.style.paddingBottom = newHeight;
 }
 
 function computedStyleNumber(element, styleProperty) {
-    return parseFloat(window.getComputedStyle(element)[styleProperty].match(/-?\d+(\.\d+)?/)[0])
+    return parseFloat(window.getComputedStyle(element)[styleProperty].replace(/\D/g, "")[0]);
 }
