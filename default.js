cc8728d811fb72249d64b3ed5fc64b1664880b65
João Victor Olivo <jvso@yahoo.com>
2024-05-12 17:10:06 -0300

    navbar + theme switch

--- default.js
+++ default.js
@@ -4,12 +4,28 @@ window.addEventListener('resize', updateResizeBased);
 function updateResizeBased() {
     content = document.querySelector("#content-wrap");
     footer = document.querySelector(".footer");
-    
+
     const newHeight = window.computedStyleNumber(footer, "height") + window.computedStyleNumber(content, "paddingTop") + "px";
-    
+
     content.style.paddingBottom = newHeight;
 }
 
 function computedStyleNumber(element, styleProperty) {
     return parseFloat(window.getComputedStyle(element)[styleProperty].replace(/\D/g, ""));
 }
+
+document.querySelector("#appearance-switch").addEventListener("click", () => {
+    if (document.documentElement.classList.contains("light")) {
+        document.documentElement.classList.remove("light")
+        document.documentElement.classList.add("dark")
+    } else if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark")
+        document.documentElement.classList.add("light")
+    } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.classList.add("dark")
+        } else {
+            document.documentElement.classList.add("light")
+        }
+    }
+})
