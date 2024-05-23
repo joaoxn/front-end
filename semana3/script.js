673c673785824bcf532dad493063090911241e41
João Victor Olivo <jvso@yahoo.com>
2024-05-22 22:27:30 -0300

    style adapting

--- semana3/script.js
+++ semana3/script.js
@@ -1,10 +1,11 @@
+/// Esse é o código JavaScript responsável por preencher a tag <main> ///
+
 let nome;
 
-if(confirm("Deseja rodar o JavaScript?")) {
-aula2();
-aula3();
-}
-/// MINHA BIBLIOTECA DE FUNÇÕES /// {
+    aula2();
+    aula3();
+
+/// MINHA BIBLIOTECA DE FUNÇÕES ///
 
 function escreverNaTela(textoFormatado, condicao, motivo) {
     // textoFormatado exibe apenas se condicao == true || condicao === undefined
@@ -19,50 +20,49 @@ function escreverNaTela(textoFormatado, condicao, motivo) {
     }
 }
 
-/// MINHA BIBLIOTECA DE FUNÇÕES /// }
-
-/// AULA 2 /// {
+/// AULA 2 ///
 function aula2() {
     escreverNaTela("<h1 class='text-topic' style='margin-top: 0;'>Aula 2</h1>");
+    { // Exercício: Convite de Casamento
+        escreverNaTela("<h2>Exercício: Convite de Casamento</h2>");
+        nome = window.prompt("Digite seu nome");
 
-    nome = window.prompt("Digite seu nome");
-
-    escreverNaTela(prepararConvite(nome), nome, "Seu nome é necessário para inserir no convite!");
+        escreverNaTela(prepararConvite(nome), nome, "Seu nome é necessário para inserir no convite!");
+    }
 }
 
 function prepararConvite(nome) {
-    return `<h4 style="background: var(--gradient-emphasis); border-radius: 1rem; padding: 1rem;">${nome}, é com muito orgulho que convido-lhe para meu casamento.</h4>`;
+    return `<p style="background-color: var(--background-secondary); border-radius: 1rem; padding: 1rem;">${nome}, é com muito orgulho que convido-lhe para meu casamento.</p>`;
 }
-/// AULA 2 /// }
 
-/// AULA 3 /// {
+/// AULA 3 ///
 function aula3() {
     escreverNaTela("<h1 class='text-topic'>Aula 3</h1>", true);
-    { // Exercício com if else e cálculo
+    { // Exercício: Cálculo de Média
+        escreverNaTela("<h2>Exercício: Cálculo de Média</h2>");
         let soma = 0;
         let i;
         let notas = parseInt(window.prompt("Quantas notas avaliativas você quer utilizar para calcular sua média?"));
-        for(i = 0; i < notas; i++) {
-            let notaInput = window.prompt(`Digite sua ${i+1}ª nota`);
-            
+        for (i = 0; i < notas; i++) {
+            let notaInput = window.prompt(`Digite sua ${i + 1}ª nota`);
+
             if (notaInput == null) {
                 break;
             } else if (isNaN(parseFloat(notaInput))) {
                 alert("Entrada inválida! Digite um número inteiro ou decimal!");
                 i--;
                 continue;
-             } else {
+            } else {
                 soma += parseFloat(notaInput);
-             }
+            }
         }
 
         let media = soma / i;
 
         if (media >= 7) {
-            escreverNaTela(`<h4 style="background: var(--gradient-emphasis); border-radius: 1rem; padding: 1rem;">Parabéns, ${nome}! Você superou a média mínima, não esqueça de continuar estudando! :) Média: ${media}</h3>`, nome, "Seu nome é necessário para exibir essa mensagem!");
+            escreverNaTela(`<p style="background-color: var(--background-secondary); border-radius: 1rem; padding: 1rem;">Parabéns, ${nome}! Você superou a média mínima, não esqueça de continuar estudando! :) Média: ${media}</h3>`, nome, "Seu nome é necessário para exibir essa mensagem!");
         } else {
-            escreverNaTela(`<h4 style="background: var(--gradient-emphasis); border-radius: 1rem; padding: 1rem;">Infelizmente, você não alcançou a média mínima. Média: ${media}</h3>`, media, "A média é necessária para calcular seu desempenho!");
+            escreverNaTela(`<p style="background-color: var(--background-secondary); border-radius: 1rem; padding: 1rem;">Infelizmente, você não alcançou a média mínima. Média: ${media}</h3>`, media, "A média é necessária para calcular seu desempenho!");
         }
     }
 }
-/// AULA 3 /// }
